ad549dc0ce864e467d218d739b559bc0197a3958
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-04-16 22:09:35 -0400

    Finished checkbox testable functionality

--- src/components/CheckBoxComp.js
+++ src/components/CheckBoxComp.js
@@ -4,23 +4,22 @@ import React from "react"
 function CheckBox() {
     //Here is where we store javascript functions
     const check = () =>{
-
         // Get the checkbox
         var checkBox = document.getElementById("check1");
 
         // If the checkbox is checked, display the output text
-        if (checkBox.checked == true){
-
+        if (checkBox.checked === true){
+            document.getElementById("checklabel").style.visibility = "visible";
         } else {
-
+            document.getElementById("checklabel").style.visibility = "hidden";
         }
     };
     //Gatsby turns the following "html" into react javascript.
     return (
     <div>
         <h1>Checkbox</h1>
-        <input type="checkbox" id="check1" onclick={check} checked></input>
-        <label for="check1"> Visible when checked!</label><br />
+        <input type="checkbox" id="check1" onChange={check} defaultChecked></input>
+        <label htmlFor="check1" id="checklabel"> Visible when checked!</label><br />
     </div>
     )
 }
