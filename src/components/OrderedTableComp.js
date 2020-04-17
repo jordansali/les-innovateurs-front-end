1c7a872889be9bf1ee66e893bd2e5a442841401a
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-04-17 09:46:45 -0400

    Able to access value of slected option

--- src/components/OrderedTableComp.js
+++ src/components/OrderedTableComp.js
@@ -4,15 +4,29 @@ import React from "react"
 function OrderedTable() {
     //Here is where we store javascript functions
     const order = () =>{
-
+        var selected = document.getElementById("order").value;
+        
+        switch(selected) {
+            case "Unordered":
+                alert("unooordered");
+                break;
+            case "AlphaDescending":
+                alert("alpha descennnd");
+                break;
+            case "AlphaAscending":
+                alert("alpha ascennnd");
+                break;
+            default:
+                alert("default");
+        } //end switch
     };
     //Gatsby turns the following "html" into react javascript.
     return (
     <div>
         <h1>Ordered Table</h1>
-        <label for="order">Choose a sort: </label>
+        <label htmlFor="order">Choose a sort: </label>
 
-        <select id="order">
+        <select id="order" onChange={order}> 
             <option value="Unordered">Unordered</option>
             <option value="AlphaDescending">Alphabetical - Descending</option>
             <option value="AlphaAscending">Alphabetical - Ascending</option>
