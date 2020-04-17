a3ade61af73e55950ca68c878e1562005a878ba7
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-04-16 21:03:40 -0400

    small refactor

--- src/components/CalcComp.js
+++ src/components/CalcComp.js
@@ -2,13 +2,15 @@ import React from "react"
 
 
 function Calculator() {
+    //Here is where we store javascript functions
     const calculate = () =>{
         var num1 = parseFloat(document.getElementById("num1").value);
         var num2 = parseFloat(document.getElementById("num2").value);
         var sum1 = num1+num2;
-        
+
         document.getElementById("sum1").innerHTML = sum1;
     };
+    //Gatsby turns the following "html" into react javascript.
     return (
         <div>
         <h1>Calculator</h1>
@@ -19,9 +21,10 @@ function Calculator() {
         <input type="number" id="num2" name="num2"/><br/><br/>
 
         <button onClick={calculate} id="btn1">Add</button><br/><br/>
-        <label id="sum1">0.0</label>
+        <label htmlFor="btn1" id="sum1">0.0</label>
     </div>
     )
 }
 
+//Export the component
 export default Calculator
