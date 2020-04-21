d824933c09adb7a2be3c44c71841c04e63f3791c
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-04-21 12:04:42 -0400

    Installation of styled components module and completed slider component

--- src/components/SliderComp.js
+++ src/components/SliderComp.js
@@ -1,4 +1,26 @@
 import React from "react"
+import styled from "styled-components"
+
+const Header = styled.h1`
+    color: red;
+    transition: all 1s ease-in-out;
+    -webkit-transition: all 1s ease-in-out;
+    -moz-transition: all 1s ease-in-out;
+    -o-transition: all 1s ease-in-out;
+    -ms-transition: all 1s ease-in-out;
+    position:absolute;
+    position: absolute;
+    
+
+    :hover {
+        color: blue;
+        transform: translate(3em,0);
+    -webkit-transform: translate(3em,0);
+    -moz-transform: translate(3em,0);
+    -o-transform: translate(3em,0);
+    -ms-transform: translate(3em,0);
+    }
+`
 
 
 function SliderComponent() {
@@ -8,7 +30,7 @@ function SliderComponent() {
     //Gatsby turns the following "html" into react javascript.
     return (
         <div>
-            <h1 id="header">Slider Component</h1>
+            <Header id="header">Slider Component</Header>
         </div>
     )
 }
