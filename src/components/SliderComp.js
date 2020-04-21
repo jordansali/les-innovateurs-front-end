86cb462759f37c61e15d8548a8b6609ee6a6e862
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-04-21 14:42:11 -0400

    Added slider, started functionality to print value but need to do more learning

--- src/components/SliderComp.js
+++ src/components/SliderComp.js
@@ -8,20 +8,26 @@ const Header = styled.h1`
     -moz-transition: all 1s ease-in-out;
     -o-transition: all 1s ease-in-out;
     -ms-transition: all 1s ease-in-out;
-    position:absolute;
-    position: absolute;
     
 
     :hover {
         color: blue;
         transform: translate(3em,0);
-    -webkit-transform: translate(3em,0);
-    -moz-transform: translate(3em,0);
-    -o-transform: translate(3em,0);
-    -ms-transform: translate(3em,0);
+        -webkit-transform: translate(3em,0);
+        -moz-transform: translate(3em,0);
+        -o-transform: translate(3em,0);
+        -ms-transform: translate(3em,0);
     }
 `
+//Need to learn more about hooks and the gatsby lifecycle
+/*function sliderVal(sliderValue) {
+    let sliderLabel = document.getElementById("sliderLabel")
+    sliderLabel.innerHTML = sliderValue;
 
+}
+function handleChange(event){
+    this.setState({value: event.target.value});
+}*/
 
 function SliderComponent() {
     //Here is where we store javascript functions
@@ -31,6 +37,8 @@ function SliderComponent() {
     return (
         <div>
             <Header id="header">Slider Component</Header>
+            <input type="range" id="slider" min="0" max="100" defaultValue="50"></input>
+            <label id="sliderLabel" htmlFor="slider"></label>
         </div>
     )
 }
