793833f55510fe6c982790281a12e2e0d4bda7f0
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-05-14 08:30:22 -0400

    Work on more endpoints

--- src/components/ApiGetComp.js
+++ src/components/ApiGetComp.js
@@ -3,15 +3,8 @@ import React from "react"
 
 function ApiGet() {
 
-    //To be sent via http Post
-    var postParameters = {
-        "categoryName_En" : "test3",
-        "categoryName_Fr" : "test trois"
-    };
-
-
     //Here is where we store javascript functions
-    function HttpGet (){
+    function HttpGet(){
         // Create a request variable and assign a new XMLHttpRequest object to it.
         var request = new XMLHttpRequest();
 
@@ -40,6 +33,12 @@ function ApiGet() {
     };
 
     function HttpPost (){
+            //To be sent via http Post
+        var postParameters = {
+            "categoryName_En" : document.getElementById("catEn").value,
+            "categoryName_Fr" : document.getElementById("catFr").value
+        };
+        
         // Create a request variable and assign a new XMLHttpRequest object to it.
         var request = new XMLHttpRequest();
 
@@ -49,11 +48,11 @@ function ApiGet() {
         
         console.log("Request Started");
 
-        document.getElementById("postResults").value = "";
+        document.getElementById("postResults").innerText = "";
 
         request.onreadystatechange = function () { //Call a function when the state changes.
             if (request.status == 201) {
-                alert(request.responseText);
+                document.getElementById("postResults").innerText = request.status + " " + request.statusText;
             }
         }
 
@@ -62,16 +61,76 @@ function ApiGet() {
         console.log("Request sent");
     };
     
+    function HttpDelete(){
+        // Create a request variable and assign a new XMLHttpRequest object to it.
+        var request = new XMLHttpRequest();
+
+        var categoryId = document.getElementById("catId").value
+        var url = `https://jeopardygame.azurewebsites.net/api/Categories/${categoryId}`
+
+        console.log("CatID: " + categoryId);
+        console.log("url: " + url);
+
+        //dont run if no numeric value is given
+        if (isNaN(categoryId)){
+            alert("Please provide a numeric value.");
+            return; 
+        }
+
+        // Create a request variable and assign a new XMLHttpRequest object to it.
+        request.open('DELETE', url, true);
+
+        console.log("Request Started");
+
+        document.getElementById("deleteResults").innerText = "";
+
+        request.onreadystatechange = function () { //Call a function when the state changes.
+            //if (request.status == 200) {
+                document.getElementById("deleteResults").innerText = request.status + " " + request.statusText;
+            //}
+        }
+
+        // Send request
+        request.send();
+        console.log("Request sent");
+
+    };
+
+    function HttpPut(){
+
+    };
     //Gatsby turns the following "html" into react javascript.
     return (
         <div>
             <h1 id="header">API Verbs</h1>
+
+            <h3>GET all</h3>
             <button id="getBtn" onClick={HttpGet}>Get</button>
             <label htmlFor="getBtn" id="getResults"></label>
-            <br />
+            <br /><br />
 
+            <h3>GET by ID</h3>
+            <button id="getIdBtn" onClick={HttpGetId}>Get</button>
+            <label htmlFor="getIdBtn" id="getIdResults"></label>
+            <br /><br />
+
+            <h3>POST</h3>
+            <label htmlFor="catEn">Category EN:</label> <br />
+            <input id="catEn" name="catEn"/><br/>
+            <label htmlFor="catFr">Category FR:</label> <br />
+            <input id="catFr" name="catFr"/><br/>
             <button id="postBtn" onClick={HttpPost}>Post</button>
             <label htmlFor="postBtn" id="postResults"></label>
+            <br /><br />
+
+            <h3>DELETE</h3>
+            <label htmlFor="catId">Category ID to delete:</label> 
+            <input type="number" id="catId" name="catId"/><br/>
+            <button id="postBtn" onClick={HttpDelete}>Delete</button>
+            <label htmlFor="deleteBtn" id="deleteResults"></label>
+            <br /><br />
+
+
         </div>
     )
 }
