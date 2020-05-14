4728766a56ec99f6c51eb35d21d8b5937ef57be9
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-05-14 10:05:38 -0400

    FInished all verbs

--- src/components/ApiGetComp.js
+++ src/components/ApiGetComp.js
@@ -7,9 +7,27 @@ function ApiGet() {
     function HttpGet(){
         // Create a request variable and assign a new XMLHttpRequest object to it.
         var request = new XMLHttpRequest();
+        var url;
+        var categoryId = null;
+
+        if (document.getElementById("getId").value.length !== 0){
+            categoryId = document.getElementById("getId").value;
+        }
+
+        document.getElementById("getId").value = null; //clear value after consuming
+
+        console.log(categoryId);
+
+        if(categoryId !== null ){
+            url = `https://jeopardygame.azurewebsites.net/api/Categories/${categoryId}`;
+        } else {
+            url = 'https://jeopardygame.azurewebsites.net/api/Categories';
+        }
+
+        console.log(url);
 
         // Open a new connection, using the GET request on the URL endpoint
-        request.open('GET', 'https://jeopardygame.azurewebsites.net/api/Categories', true);
+        request.open('GET', url, true);
         console.log("Request Started");
 
         //Clear label
@@ -20,11 +38,17 @@ function ApiGet() {
             // Begin accessing JSON data here
             var data = JSON.parse(this.response)
 
-            data.forEach(category => {
-                // Log each movie's title
-                console.log(category.categoryName_En);
-                document.getElementById("getResults").innerText += category.categoryName_En + " ";
-            });
+            if (categoryId === null){
+                data.forEach(category => {
+                    // Log each movie's title
+                    console.log(category.categoryName_En);
+                    document.getElementById("getResults").innerText += category.categoryName_En + " ";
+                });
+            } else {
+                console.log(data.categoryName_En);
+                document.getElementById("getResults").innerText += data.categoryName_En + " ";
+            }
+            
         }
 
         // Send request
@@ -97,21 +121,44 @@ function ApiGet() {
     };
 
     function HttpPut(){
+        //To be sent via http Post
+        var putParameters = {
+            "id" : document.getElementById("putId").value,
+            "categoryName_En" : document.getElementById("putCatEn").value,
+            "categoryName_Fr" : document.getElementById("putCatFr").value
+        };
+        
+        // Create a request variable and assign a new XMLHttpRequest object to it.
+        var request = new XMLHttpRequest();
 
+        // Create a request variable and assign a new XMLHttpRequest object to it.
+        request.open('PUT', `https://jeopardygame.azurewebsites.net/api/Categories/${putParameters.id}`, true);
+        request.setRequestHeader("Content-type", "application/json");
+        
+        console.log("Request Started");
+
+        document.getElementById("putResults").innerText = "";
+
+        request.onreadystatechange = function () { //Call a function when the state changes.
+            if (request.status == 204) {
+                document.getElementById("putResults").innerText = request.status + " Updated.";
+            }
+        }
+
+        // Send request
+        request.send(JSON.stringify(putParameters));
+        console.log("Request sent");
     };
     //Gatsby turns the following "html" into react javascript.
     return (
         <div>
             <h1 id="header">API Verbs</h1>
 
-            <h3>GET all</h3>
+            <h3>GET</h3>
+            <label htmlFor="getId">Category ID (optional):</label>
+            <input id="getId" name="getId"/><br/>
             <button id="getBtn" onClick={HttpGet}>Get</button>
             <label htmlFor="getBtn" id="getResults"></label>
-            <br /><br />
-
-            <h3>GET by ID</h3>
-            <button id="getIdBtn" onClick={HttpGetId}>Get</button>
-            <label htmlFor="getIdBtn" id="getIdResults"></label>
             <br /><br />
 
             <h3>POST</h3>
@@ -121,6 +168,17 @@ function ApiGet() {
             <input id="catFr" name="catFr"/><br/>
             <button id="postBtn" onClick={HttpPost}>Post</button>
             <label htmlFor="postBtn" id="postResults"></label>
+            <br /><br />
+
+            <h3>PUT</h3>
+            <label htmlFor="putId">Category ID:</label> <br />
+            <input id="putId" name="putId"/><br/>
+            <label htmlFor="putCatEn">Category EN:</label> <br />
+            <input id="putCatEn" name="putCatEn"/><br/>
+            <label htmlFor="putCatFr">Category FR:</label> <br />
+            <input id="putCatFr" name="putCatFr"/><br/>
+            <button id="putBtn" onClick={HttpPut}>Update</button>
+            <label htmlFor="putBtn" id="putResults"></label>
             <br /><br />
 
             <h3>DELETE</h3>
