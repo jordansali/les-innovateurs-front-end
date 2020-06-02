36556366dc61f94646b7bf3191222e6b881ef77e
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-06-02 13:37:59 -0400

    Started user entry page

---
+++ src/pages/start.js
@@ -0,0 +1,10 @@
+/*Component names must start with a capital */
+import React from "react"
+
+/*Any React component defined in src/pages/*.js will automatically become a page.*/
+/*Everything within 'export' must have one parent tag */
+export default () => (
+    <div>
+
+    </div>
+)
